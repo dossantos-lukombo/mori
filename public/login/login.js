6c3384ff89e655cb0956b620a660b66e08baaa17
Daryl PARISI <dparisi@groupecandor.fr>
2024-11-18 10:11:48 +0100

    New_login/register

---
+++ public/login/login.js
@@ -0,0 +1,11 @@
+function toggleForms() {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'flex';
+        registerForm.style.display = 'none';
+    } else {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'flex';
+    }
+}
