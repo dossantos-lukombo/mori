81d405a9e1e25c2dbc697bb1891211ff767c1fc8
Santos <santoslukombo@gmail.com>
2024-11-19 14:54:28 +0100

    a lot of changes, jwt python side test working

---
+++ backend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"mori/app"
+	"mori/captcha"
+	"mori/database"
+	login "mori/loginBack"
+	"net/http"
+)
+
+func main() {
+
+	// Initialize the database
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer db.Close()
+
+	fmt.Println("Successfully connected to the database!")
+
+	// Define routes
+	http.HandleFunc("/login", login.LoginHandler(db))
+	http.HandleFunc("/register", login.RegisterHandler(db))
+	http.HandleFunc("/captcha", captcha.ServeCaptcha)
+	http.HandleFunc("/protected", login.AuthMiddleware(db, protectedHandler))
+	http.HandleFunc("/verify-email", login.VerifyEmailHandler(db))
+	// API route for llm call
+
+	// Start the server
+	app.StartServer()
+}
+
+// Example protected route
+func protectedHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "You have accessed a protected route!")
+}
