9f9e2a85bd30ddf7a940f725811f2bf9bfd953de
DarylParisi <pharisidharyl_10@yahoo.fr>
2024-11-23 23:22:21 +0100

    global Var for router

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,8 @@ import (
 	"fmt"
 	"log"
 	"mori/app"
-	"mori/captcha"
 	"mori/database"
-	login "mori/loginBack"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	config "mori/router"
 )
 
 func main() {
@@ -22,34 +18,9 @@ func main() {
 
 	fmt.Println("Successfully connected to the database!")
 
-	// Initialize the router
-	router := mux.NewRouter()
-
-	// Define API routes
-	router.HandleFunc("/login", login.LoginHandler(db))
-	router.HandleFunc("/register", login.RegisterHandler(db))
-	router.HandleFunc("/captcha", captcha.ServeCaptcha)
-	router.HandleFunc("/verify-email", login.VerifyEmailHandler(db))
-	router.HandleFunc("/reset-password", login.ResetPasswordHandler(db))
-	router.HandleFunc("/reset-password-form", login.ServeResetPasswordForm(db))
-	router.HandleFunc("/verify-reset-token", login.VerifyResetTokenHandler(db))
-
-	// Protected routes
-	protectedRoutes := router.PathPrefix("/protected").Subrouter()
-	protectedRoutes.Use(login.AuthMiddleware(db))
-	protectedRoutes.HandleFunc("", protectedHandler)
-
-	// Static file serving
-	router.PathPrefix("/frontend/").Handler(http.StripPrefix("/frontend/", http.FileServer(http.Dir("../frontend"))))
-
-	// Root route
-	router.HandleFunc("/", app.LoginPageHandler)
+	// Initialize the router and pass the db object
+	config.InitializeRouter(db)
 
 	// Pass the router to server.go to start the server
-	app.StartServer(router)
-}
-
-// Example protected route
-func protectedHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "You have accessed a protected route!")
+	app.StartServer(config.Router)
 }
---
+++ backend/router/config.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"database/sql"
+	"mori/app"
+	"mori/captcha"
+	loginback "mori/loginBack"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Router will be used across the project
+var Router *mux.Router
+
+// InitializeRouter sets up the router
+func InitializeRouter(db *sql.DB) {
+	// Create a new router
+	Router = mux.NewRouter()
+
+	// Define API routes
+	Router.HandleFunc("/login", loginback.LoginHandler(db))
+	Router.HandleFunc("/register", loginback.RegisterHandler(db))
+	Router.HandleFunc("/captcha", captcha.ServeCaptcha)
+	Router.HandleFunc("/verify-email", loginback.VerifyEmailHandler(db))
+	Router.HandleFunc("/reset-password", loginback.ResetPasswordHandler(db))
+	Router.HandleFunc("/reset-password-form", loginback.ServeResetPasswordForm(db))
+	Router.HandleFunc("/verify-reset-token", loginback.VerifyResetTokenHandler(db))
+
+	// Protected routes
+	protectedRoutes := Router.PathPrefix("/protected").Subrouter()
+	protectedRoutes.Use(loginback.AuthMiddleware(db))
+
+	// Static file serving
+	Router.PathPrefix("/frontend/").Handler(http.StripPrefix("/frontend/", http.FileServer(http.Dir("../frontend"))))
+
+	// Root route
+	Router.HandleFunc("/", app.LoginPageHandler)
+}
