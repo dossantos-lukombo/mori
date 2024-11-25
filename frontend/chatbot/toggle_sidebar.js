b29645992ab48ef5fed16fb774a64ad320eac30c
Daryl PARISI <dparisi@groupecandor.fr>
2024-11-25 13:31:17 +0100

    chatbot animations

--- frontend/chatbot/toggle_sidebar.js
+++ frontend/chatbot/toggle_sidebar.js
@@ -4,14 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const chatContainer = document.querySelector('.chat-container');
     const chatWindow = document.querySelector('.chat-window');
     const user_input = document.querySelector('.chat-input');
-  
+    const logo = document.querySelector('.small-logo');
+    const sidebar_header = document.querySelector('.sidebar-header');
+    
     toggleButton.addEventListener('click', function() {
         sidebar.classList.toggle('hidden');
         chatContainer.classList.toggle('full-width');
         chatWindow.scrollTop = chatWindow.scrollHeight;
         chatWindow.classList.toggle('full-width');
         user_input.classList.toggle('full-width');
+        logo.classList.toggle('full-width');
 
     });
-  });
+});
   
