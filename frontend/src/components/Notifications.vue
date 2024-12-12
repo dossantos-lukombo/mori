d17a2701c4f74d2e4295e6f5692f62f8448963ae
Quentin <boiteuxquentin@gmail.com>
2024-12-12 16:06:17 +0100

    delete postand end event

--- frontend/src/components/Notifications.vue
+++ frontend/src/components/Notifications.vue
@@ -1,37 +1,24 @@
 <template>
-
     <div class="relative-wrapper" @click="toggleShowNotifications">
         <span class="link-header">Notifications
             <div class="notification-ring" v-show="hasNotifications"></div>
-
         </span>
         <div class="item-list__wrapper" id="notifications" v-show="showNotifications">
             <ul class="item-list">
-                <li v-for="notification in allNotifications" :key="notification.id"
-                    v-if="hasNotifications">
-
-                    <div class="row1 ">
+                <li v-for="notification in allNotifications" :key="notification.id" v-if="hasNotifications">
+                    <div class="row1">
                         <img class="" src="../assets/icons/default-profile.svg">
                         <NotificationMsg :notification="notification"></NotificationMsg>
                     </div>
-
-                    <!--<div class="row2" v-if="notification.type === 'EVENT'">
-                         <i class="uil uil-times decline" @click.stop="handleEventRequest(notification, 'NO')"></i>
-                        <i class="uil uil-check accept" @click.stop="handleEventRequest(notification, 'YES')"></i>
-                    </div>
-
-                    <div class="row2" v-else>
+                    <div class="row2">
                         <i class="uil uil-times decline" @click.stop="handleRequest(notification, 'decline')"></i>
                         <i class="uil uil-check accept" @click.stop="handleRequest(notification, 'accept')"></i>
                     </div>-->
                 </li>
-
                 <li v-else class="additional-info">No notifications</li>
-
             </ul>
         </div>
     </div>
-
 </template>
 
 <script>
@@ -54,11 +41,7 @@ export default {
             allNotifications: state => state.notifications.allNotifications
         }),
         hasNotifications() {
-            if (this.allNotifications === null) {
-                return false;
-            } else {
-                return this.allNotifications.length > 0;
-            }
+            return this.allNotifications?.length > 0;
         }
     },
     unmounted() {
@@ -69,7 +52,6 @@ export default {
         toggleShowNotifications() {
             this.showNotifications = !this.showNotifications;
         },
-
         async fetchNotifications() {
             const response = await fetch("http://localhost:8081/notifications", {
                 credentials: "include"
@@ -82,8 +64,6 @@ export default {
                 this.notificationsFromDB = data;
                 this.$store.commit("updateAllNotifications", data.notifications);
             }
-           
-            // console.log("/notifications data", data)
         },
         async handleRequest(notification, reqResponse) {
             let endpoint;
@@ -95,10 +75,10 @@ export default {
                     endpoint = "responseInviteRequest";
                     break;
                 case "GROUP_REQUEST":
-                    endpoint = "responseGroupRequest"
+                    endpoint = "responseGroupRequest";
                     break;
                 case "CHAT_REQUEST":
-                    endpoint = "responseChatRequest"
+                    endpoint = "responseChatRequest";
                     break;
             }
             const response = await fetch(`http://localhost:8081/${endpoint}`, {
@@ -107,29 +87,21 @@ export default {
                 body: JSON.stringify({
                     requestId: notification.id,
                     response: reqResponse,
-                    groupId: notification.group.id
+                    groupId: notification.group?.id
                 })
             });
             const data = await response.json();
-            // console.log("/handleRequest data", data)
+
             if (notification.type === "GROUP_INVITE" && reqResponse === "accept") {
-                // update user groups for live update
                 this.$store.dispatch("addUserGroup", notification.group);
             }
 
-            if (notification.type === "CHAT_REQUEST" || notification.type == "FOLLOW" && reqResponse === "accept") {
-                // update user groups for live update
-                // console.log("NOTIFICATION", notification)
+            if ((notification.type === "CHAT_REQUEST" || notification.type === "FOLLOW") && reqResponse === "accept") {
                 this.$store.dispatch("fetchChatUserList");
                 this.$store.dispatch("fetchUnreadMessages");
-                
             }
 
-            // if (notification.type === "GROUP_INVITE" && reqResponse === "accept")
-            // remove the notification
             this.$store.dispatch("removeNotification", notification.id);
-
-            // this.checkNotificationsLength();
 
             if (!this.hasNotifications) {
                 this.toggleShowNotifications();
@@ -150,16 +122,13 @@ export default {
         }
     },
     components: { NotificationMsg }
-}
-
+};
 </script>
 
 <style>
 .relative-wrapper {
     position: relative;
-
 }
-
 
 #notifications .row1 :is(img, i) {
     height: 2.25em;
@@ -167,7 +136,6 @@ export default {
 }
 
 #notifications {
-
     position: absolute;
     transform: translateX(-50%);
     left: 50%;
@@ -176,8 +144,6 @@ export default {
     width: 400px;
     cursor: default;
 }
-
-
 
 #notifications .item-list {
     gap: 15px;
@@ -188,20 +154,16 @@ export default {
     gap: 20px;
 }
 
-
 .who {
     font-weight: 500;
 }
-
 
 .link-header::after {
     content: "";
     height: 2.5px;
     width: 0;
     display: block;
-
     position: absolute;
-
     transition: all 0.35s ease-in-out;
 }
 
