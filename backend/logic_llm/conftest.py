7a0de48bccbfe7e06473f0795cf4f18b0072280e
dossantos-lukombo <dossantos.lukombo918@gmail.com>
2025-05-29 17:58:33 +0200

    chore: editing pytest

--- backend/logic_llm/conftest.py
+++ backend/logic_llm/conftest.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv, find_dotenv
 
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
-    env_path = find_dotenv("../.env")
+    env_path = find_dotenv(".env")
     if not load_dotenv(env_path):
         pytest.exit("❌ Impossible de charger le .env", returncode=1)
